14c4c9d7e350e7f2ff98061d70fd7f9504383905
Lockric <88631057+Lockric@users.noreply.github.com>
2025-09-16 12:04:54 -0400

    rename outputs
    - still needs further configurations

--- src/ppu_to_llhdmi.v
+++ src/ppu_to_llhdmi.v
@@ -1,13 +1,13 @@
 module ppu_to_llhdmi(input clk,
-                     output reg vga_h, output reg vga_v,
-                     output reg [3:0] vga_r, output reg[3:0] vga_g, output reg[3:0] vga_b,
+                     output reg ppu_h, output reg ppu_v,
+                     output reg [7:0] hdmi_r, output reg[7:0] hdmi_g, output reg[7:0] hdim_b,
                      output [9:0] vga_hcounter,
                      output [9:0] vga_vcounter,
                      output [9:0] next_pixel_x, // The pixel we need NEXT cycle.
                      input [14:0] pixel,        // Pixel for current cycle.
                      input sync,
                      input border);
-
+    // inputs and outputs still need further configurtions
 
 
 endmodule
